5af58f88fdb1831c418e1131ae9c122699351dd7
Samuel López <44242803+SamShelby@users.noreply.github.com>
2022-01-10 15:55:58 -0600

    test lan

--- src/scripts/test_lan_detect.py
+++ src/scripts/test_lan_detect.py
@@ -1,6 +1,24 @@
 from textblob import TextBlob
 
+def detect_language(text):
+    """Detects the text's language."""
+    from google.cloud import translate_v2 as translate
+
+    translate_client = translate.Client()
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.detect_language(text)
+
+    print("Text: {}".format(text))
+    print("Confidence: {}".format(result["confidence"]))
+    print("Language: {}".format(result["language"]))
+
+from googletrans import Translator
 
 if __name__ == '__main__':
+    translator = Translator()
     line = 'frase de prueba'
-    print(TextBlob(str(line)).detect_language())
+    # print(detect_language(line))
+    print(translator.detect(line))
+    # print(TextBlob(str(line)).detect_language())
